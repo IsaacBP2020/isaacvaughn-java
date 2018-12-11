f4f07588a011c387a12811ca1c1e4622e02350f0
IsaacBP2020 <20vaughnisaac@bprep.org>
2018-12-11 08:47:15 -0800

    finish

---
+++ rps/src/rps.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class rps {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("do you want to play a game? \"y\" or \"n\"");
+        String play = keyboard.nextLine();
+        System.out.println("what is your name");
+        String name = keyboard.nextLine();
+        System.out.println("\"3\" or \"5\" rounds");
+        Integer rounds = keyboard.nextInt();
+        System.out.println("0=rock\n1=paper\n2=scissors");
+
+    }
+
+public static void game(){
+
+}
+    public static void player() {
+
+        }
+
+
+}
